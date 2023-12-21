b46501b06371f7b2f8b3542ac6552aad01be9e8f
dzaba1 <blazej.zabialowicz@gmail.com>
2023-12-21 21:54:37 +0100

    Enumerable type alias

--- src/enumerable.ts
+++ src/enumerable.ts
@@ -1,5 +1,7 @@
 export module Enumerable {
-    export function *where<T>(gen: Generator<T>, predicate: (elem: T) => boolean): Generator<T> {
+    export type EnumerableType<T> = Generator<T> | Array<T>
+
+    export function *where<T>(gen: EnumerableType<T>, predicate: (elem: T) => boolean): Generator<T> {
         for (const item of gen) {
             if (predicate(item)) {
                 yield item;
@@ -7,25 +9,32 @@ export module Enumerable {
         }
     }
 
-    export function *select<TIn, TOut>(gen: Generator<TIn>, selector: (elem: TIn) => TOut): Generator<TOut> {
+    export function *select<TIn, TOut>(gen: EnumerableType<TIn>, selector: (elem: TIn) => TOut): Generator<TOut> {
         for (const item of gen) {
             yield selector(item);
         }
     }
 
-    export function any<T>(gen: Generator<T>): boolean {
+    export function any<T>(gen:EnumerableType<T>): boolean {
         for (const {} of gen) {
             return true;
         }
         return false;
     }
 
-    export function all<T>(gen: Generator<T>, predicate: (elem: T) => boolean): boolean {
+    export function all<T>(gen: EnumerableType<T>, predicate: (elem: T) => boolean): boolean {
         for (const item of gen) {
             if (!predicate(item)) {
                 return false;
             }
         }
         return true;
+    }
+
+    export function first<T>(gen: EnumerableType<T>): T {
+        for (const item of gen) {
+            return item;
+        }
+        throw "Empty sequence";
     }
 }
